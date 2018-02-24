7600d75349fb10a469c650895affb416a29a9cd7
Aver JIng <panda12396@163.com>
2018-02-24 19:49:37 +0800

    CH8 excercise

---
+++ Chapter8/Chapter8/Sales_data.h
@@ -0,0 +1,79 @@
+#ifndef SALES_DATA
+#define SALES_DATA
+#include <string>
+
+using namespace std;
+
+struct Sales_data
+{
+	friend ostream & operator<<(ostream &os, const Sales_data &val) {
+		os << val.bookNo << ' ' << val.units_sold << ' '
+			<< val.aver_price << ' ' << val.revenue << endl;
+		return os;
+	}
+	friend istream & operator >>(istream &in, Sales_data &val) {
+		in >> val.bookNo >> val.units_sold >> val.aver_price;
+		val.revenue = val.units_sold * val.aver_price;
+		return in;
+	}
+	friend istream &read(istream &in, Sales_data &val) {
+		in >> val.bookNo >> val.units_sold >> val.aver_price;
+		val.revenue = val.units_sold * val.aver_price;
+		return in;
+	}
+	friend ostream &print(ostream &os, Sales_data &val) {
+		os << val.bookNo << ' ' << val.units_sold << ' '
+			<< val.aver_price << ' ' << val.revenue;
+		return os;
+	}
+private:
+	string bookNo;
+	double revenue = 0.0;
+	unsigned units_sold = 0;
+	double aver_price = 0.0;
+	inline double avg_price() const
+	{
+		return units_sold ? revenue / units_sold : 0;
+	}
+public:
+	//ex7_12 Define a constructor that takes an istream into the body of this class
+	Sales_data(istream &is) : Sales_data() {
+		read(is, *this);
+	}
+	/*Sales_data(const string book, const unsigned num, const double price) :bookNo(book),
+		units_sold(num), revenue(price * num),aver_price(price) {}*/
+	Sales_data(const std::string& s, unsigned n, double p)
+		: bookNo(s), units_sold(n), revenue(n * p)
+	{
+		//std::cout << "Sales_data(const std::string&, unsigned, double)"
+			//<< std::endl;
+	}
+
+	Sales_data() : Sales_data("", 0, 0.0f)
+	{
+		//std::cout << "Sales_data()" << std::endl;
+	}
+
+	Sales_data(const std::string& s) : Sales_data(s, 0, 0.0f)
+	{
+		//std::cout << "Sales_data(const std::string&)" << std::endl;
+	}
+
+	string ISBN() const{
+		return bookNo;
+	}
+	Sales_data & operator +=(const Sales_data& val_data) {
+		revenue += val_data.revenue;
+		units_sold += val_data.units_sold;
+		aver_price = revenue / units_sold;
+		return *this;
+	}
+	Sales_data& combine(const Sales_data  &rhs) {
+		revenue += rhs.revenue;
+		units_sold += rhs.units_sold;
+		return *this;
+	}
+};
+
+
+#endif // !SALES_DATA
---
+++ Chapter8/Chapter8/ex8_part.cpp
@@ -0,0 +1,229 @@
+/*
+*
+*@author: AverJing
+*@description: a part of excercise in CH8
+*@date:Saturday, February 24, 2018
+*
+*/
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "Sales_data.h"
+
+using namespace std;
+
+//Write a function that takes and returns an istream &
+istream &ex8_1(istream &is) {
+	string str;
+	while (is >> str)
+		cout << str << endl;
+	is.clear();//reset  the stream
+	return is;
+}
+
+//Write a function to open a file for input and read its contents into a vector
+//of strings, storing each line as a separate element in the vector.
+void ex8_4(const string &fileName, vector<string> &svec) {
+	ifstream ifile(fileName, ios::in);
+	
+	if (ifile) {
+		string str;
+		while (getline(ifile, str) ){
+			svec.push_back(str);
+		}
+	}	
+}
+
+//Rewrite the previous program to store each word in separate element
+void ex8_5(const string &fileName, vector<string> &svec) {
+	ifstream ifile(fileName, ios::in);
+	if (ifile) {	
+		string str;
+		while (ifile >> str) {
+			svec.push_back(str);
+		}
+	}
+}
+
+//Rewrite the bookstore program to read its transactions from a file.
+void ex8_6(const string &fileName) {
+	ifstream input(fileName);
+	Sales_data total;
+	if (read(input, total)) {
+		Sales_data trans;
+		while (read(input, trans)) {
+			if (total.ISBN() == trans.ISBN())
+				total.combine(trans);
+			else {
+				print(cout, total) << endl;
+				total = trans;
+			}
+		}
+		print(cout, total) << endl;
+	}
+	else {
+		cerr << "No data?!" << endl;
+	}
+}
+
+//Revise the previous program to write its output to a file.
+void ex8_7(const string &ifile, const string &ofile) {
+	ifstream input(ifile);
+	ofstream output(ofile);
+	Sales_data total;
+	if (read(input, total)) {
+		Sales_data trans;
+		while (read(input, trans)) {
+			if (total.ISBN() == trans.ISBN())
+				total.combine(trans);
+			else {
+				print(output, total) << endl;
+				total = trans;
+			}
+		}
+		print(output, total) << endl;
+	}
+	else {
+		cerr << "No data?!" << endl;
+	}
+}
+
+//Revise the previous program to append its output to its given file.
+//Run this program on the same output file at least twice to ensure the data are preserved. 
+void ex8_8(const string &ifile, const string &ofile) {
+	ifstream input(ifile);
+	ofstream output(ofile, ios::app);
+	Sales_data total;
+	if (read(input, total)) {
+		Sales_data trans;
+		while (read(input, trans)) {
+			if (total.ISBN() == trans.ISBN())
+				total.combine(trans);
+			else {
+				print(output, total) << endl;
+				total = trans;
+			}
+		}
+		print(output, total) << endl;
+	}
+	else {
+		cerr << "No data?!" << endl;
+	}
+}
+
+//Write a program to store each line from a file in a vector<string>.
+//Use isstringstream to read each element from the vector a word at a time.
+//********** unfamiliar
+void ex8_10(const string &fileName, vector<string> &svec) {
+	ifstream ifle(fileName);
+	if (ifle) {
+		string str;
+		while (getline(ifle, str))
+			svec.push_back(str);
+	}
+	ifle.close();
+	for (auto &str : svec) {
+		string word;
+		istringstream iss(str);
+		while (iss >> word) cout << word << ' ';
+		cout << endl;
+	}
+}
+
+struct PersonInfo {
+	string name;
+	vector<string> phones;
+};
+
+void ex8_11(const string &fileName) {
+	ifstream ifle(fileName);
+	string line, word;
+	vector<PersonInfo> people;
+	istringstream record;
+	while (getline(ifle, line)) {
+		PersonInfo info;
+		record.clear();
+		record.str(line);
+		record >> info.name;
+		while (record >> word) info.phones.push_back(word);
+		people.push_back(info);
+	}
+
+	for (auto& p : people) {
+		cout << p.name << " ";
+		for (auto& s : p.phones) cout << s << " ";
+		cout << endl;
+	}
+}
+
+bool valid(const string& str)
+{
+	return isdigit(str[0]);
+}
+
+string format(const string& str)
+{
+	return str.substr(0, 3) + "-" + str.substr(3, 3) + "-" + str.substr(6);
+}
+//Rewrite the phone number program from this section  to read from  a named file 
+//rather than from cin.
+void ex8_13(const string &fileName) {
+	ifstream ifle(fileName);
+	string line, word;
+	vector<PersonInfo> people;
+	istringstream record;
+	while (getline(ifle, line)) {
+		PersonInfo info;
+		record.clear();
+		record.str(line);
+		record >> info.name;
+		while (record >> word) info.phones.push_back(word);
+		people.push_back(info);
+	}
+
+	for (auto& p : people) {
+		cout << p.name << " ";
+		for (auto& s : p.phones) cout << s << " ";
+		cout << endl;
+	}
+
+	for (const auto& entry : people) {
+		ostringstream formatted, badNums;
+		for (const auto& nums : entry.phones)
+			if (!valid(nums))
+				badNums << " " << nums;
+			else
+				formatted << " " << format(nums);
+		if (badNums.str().empty())
+			cout << entry.name << " " << formatted.str() << endl;
+		else
+			cerr << "input error: " << entry.name << " invalid number(s) "
+			<< badNums.str() << endl;
+	}
+}
+
+int main(){
+	//ex8_1(cin);
+
+	vector<string> svec;
+	////ex8_4("input.txt", svec);
+	//ex8_5("input.txt", svec);
+
+	//for (auto begin = svec.cbegin(), end = svec.cend(); begin != end; ++begin)
+	//	cout << *begin << endl;
+
+	//ex8_6("data.txt");
+	//ex8_7("data.txt", "result.txt");
+	//ex8_8("data.txt", "result.txt");
+
+	/*istringstream istr("hello");
+	ex8_1(istr);*/
+
+	//ex8_10("input.txt", svec);
+
+	//ex8_11("Person.txt");
+	ex8_13("Person.txt");
+	return 0;
+}
