3152001e1a33dc2f665fcfb116a3f4cfdb0ac6e1
Aver JIng <panda12396@163.com>
2018-09-09 20:53:22 +0800

    fix CH02 exercises partly

---
+++ Chapter2/Chapter2/ex02_04.cpp
@@ -0,0 +1,27 @@
+/*
+*
+*
+*@author: Aver Jing
+*@description：
+*@date：
+*
+*
+*/
+
+#include <iostream>
+
+void ex02_03() {
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl;
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+	std::cout << i - i2 << std::endl;
+	std::cout << i - u << std::endl;
+	//std::cout << i - u2 << std::endl;
+	std::cout << u - i << std::endl;
+}
+
+int main(){
+	int i = 09;
+}	
---
+++ Chapter2/Chapter2/test.cpp
@@ -0,0 +1,17 @@
+/*
+*
+*
+*@author: Aver Jing
+*@description：
+*@date：
+*
+*
+*/
+
+#include <iostream>
+
+
+
+int main(){
+	ex02_03();
+}	
